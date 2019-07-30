32dd9504f6cbcbe7d88b45692cf6d63373f731e9
Darin <darindev1000@gmail.com>
2019-07-30 16:56:20 -0700

    Initial changes

--- handler.ts
+++ handler.ts
@@ -1,10 +1,9 @@
-import * as Ask from 'ask-sdk';
-import 'source-map-support/register';
+import * as Ask from "ask-sdk";
+import "source-map-support/register";
 
 export const alexa = Ask.SkillBuilders.custom()
   .addRequestHandlers({
-    canHandle: handlerInput => true,
-    handle: handlerInput =>
-      handlerInput.responseBuilder.speak('Hello world!').getResponse()
+    canHandle: (handlerInput) => true,
+    handle: (handlerInput) => handlerInput.responseBuilder.speak("Hello world!").getResponse()
   })
   .lambda();
